99b68b1fc968e6f215f5115c41da6e530a480967
Christine Hutabarat <13520005@std.stei.itb.ac.id>
2022-11-30 11:33:52 +0700

    feat: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
-import { ChakraProvider, Heading } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Button, ChakraProvider, Flex, Heading, Input, useDisclosure, Image, Text, useToast } from "@chakra-ui/react";
+import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
 import theme from "./styles/theme";
+import React from "react";
+import { AuthProvider, useAuth } from "./context/auth";
+import { Song, User } from "./types/models";
+import { SongTable } from "./components/songTable";
+import Penyanyi from "./pages/penyanyi";
+import Index from "./pages/index";
+import Admin from './pages/admin';
 
 function App() {
+
   return (
-    <ChakraProvider theme={theme}>
-      <Heading>Binotify Premium</Heading>
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/user' element={<Penyanyi />} />
+          <Route path='/admin' element={<Admin />} />
+        </Routes>
+        
+        </BrowserRouter>
+      </ChakraProvider>
+    </AuthProvider>
+    
   );
 }
 
